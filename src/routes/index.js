d62002583a18bf66e68f45060d31f4d297ff689a
diegoochoam <dialogo.crg@gmail.com>
2020-12-01 17:15:53 -0600

    Vista de envio

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 const home_page_router = require('./home-page');
-const login_page_controller = require('./login-page');
+const login_page_router = require('./login-page');
 const pos_page_router = require('./pos-controller');
+const shipments_page_router = require('./shipments-page');
 
 router.use('/home', home_page_router);
 
-router.use('/login', login_page_controller);
+router.use('/login', login_page_router);
 
 router.use('/pos', pos_page_router);
+
+router.use('/shipments', shipments_page_router);
 module.exports = router;
