dfebae10afd17a070712ce9cb416ff172248682f
diegoochoam <dialogo.crg@gmail.com>
2020-12-01 18:51:27 -0600

    Vista de agregar envio

--- src/routes/add-shipment-page/index.js
+++ src/routes/add-shipment-page/index.js
@@ -1,11 +1,11 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const check_format = require('../../middlewares/check-format');
-const pos_controller = require('./pos-controller');
+const add_shipment_controller = require('./add-shipment-controller');
 const router = express.Router();
 
 router.use('/', bodyParser.raw({ type: 'application/octet-stream', limit: '10MB' }));
 
-router.get('/', check_format, pos_controller);
+router.get('/', check_format, add_shipment_controller);
 
 module.exports = router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,9 @@ const router = express.Router();
 
 const home_page_router = require('./home-page');
 const login_page_router = require('./login-page');
-const pos_page_router = require('./pos-controller');
+const pos_page_router = require('./pos-page');
 const shipments_page_router = require('./shipments-page');
+const add_shipment_router = require('./add-shipment-page');
 
 router.use('/home', home_page_router);
 
@@ -13,4 +14,7 @@ router.use('/login', login_page_router);
 router.use('/pos', pos_page_router);
 
 router.use('/shipments', shipments_page_router);
+
+router.use('/add-shipment', add_shipment_router);
+
 module.exports = router;
