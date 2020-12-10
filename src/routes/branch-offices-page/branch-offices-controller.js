c518013b9bee4097f74bddb636e214d6f1548ba2
Zazil Ibarra <zaziil.97@gmail.com>
2020-12-10 15:20:15 -0600

    Sucursales distribuido

--- src/routes/branch-offices-page/branch-offices-controller.js
+++ src/routes/branch-offices-page/branch-offices-controller.js
@@ -2,21 +2,29 @@ const express = require('express');
 const app = express();
 const mysqlConnection = require('../../../connection');
 const sqlconnection = mysqlConnection.getConexion("sucursal");
-const BD = sqlconnection.BD;
-const tabla = sqlconnection.tabla;
-const condicion = sqlconnection.condicion;
 
 app.set('view engine', 'ejs');
 
 async function list(req, res) {
-  BD.query('SELECT * FROM sucursal2', (err, rows) => {
-    if (err)
-      res.json(err);
+  mysqlConnection.getConexion("sucursal")
+    .then(sqlconnection => {
+      const tabla = sqlconnection.tabla;
 
-    res.render('branch-offices', {
-      data: rows
-    });
-  });
+      sqlconnection.BD.query(`SELECT * FROM ${tabla}`, (err, rows) => {
+        if (err)
+          res.json(err);
+
+        res.render('branch-offices', {
+          data: rows
+        });
+      });
+    })
+    .catch(err => {
+      res.status(500);
+      res.render('branch-offices', {
+        data: []
+      });
+    })
 }
 
 async function add(req, res) {
@@ -28,36 +36,74 @@ async function add(req, res) {
 async function save(req, res) {
   const data = req.body;
 
-  BD.query('INSERT INTO sucursal2 SET ?', [data], (err, rows) =>{
-    res.redirect('/branch-offices');
-  });
+  mysqlConnection.getConexion("sucursal")
+    .then(sqlconnection => {
+      const tabla = sqlconnection.tabla;
+
+      sqlconnection.BD.query(`INSERT INTO ${tabla} SET ?`, [data], (err, rows) => {
+        res.redirect('/branch-offices');
+      });
+    })
+    .catch(err => {
+      res.status(500);
+      res.render('add-branch-office', {
+        data: null
+      });
+    })
 }
 
 async function _delete(req, res) {
   const id = req.params.id;
 
-  BD.query('DELETE FROM sucursal2 WHERE id = ?', [id], (err, rows) =>{
-    res.redirect('/branch-offices');
-  });
+  mysqlConnection.getConexion("sucursal")
+    .then(sqlconnection => {
+      const tabla = sqlconnection.tabla;
+
+      sqlconnection.BD.query(`DELETE FROM ${tabla} WHERE id = ?`, [id], (err, rows) => {
+        res.redirect('/branch-offices');
+      });
+    })
+    .catch(err => {
+      res.status(500);
+      res.redirect('/branch-offices');
+    })
 }
 
 async function edit(req, res) {
   const id = req.params.id;
 
-  BD.query('SELECT * FROM sucursal2 WHERE id = ?', [id], (err, rows) =>{
-    res.render('add-branch-office', {
-      data: rows[0]
-    });
-  });
+  mysqlConnection.getConexion("sucursal")
+    .then(sqlconnection => {
+      const tabla = sqlconnection.tabla;
+
+      sqlconnection.BD.query(`SELECT * FROM ${tabla} WHERE id = ?`, [id], (err, rows) => {
+        res.render('add-branch-office', {
+          data: rows[0]
+        });
+      });
+    })
+    .catch(err => {
+      res.status(500);
+      res.redirect('/branch-offices');
+    })
 }
 
 async function update(req, res) {
   const id = req.params.id;
   const data = req.body;
 
-  BD.query('UPDATE sucursal2 SET ? WHERE id = ?', [data, id], (err, rows) =>{
-    res.redirect('/branch-offices');
-  });
+  mysqlConnection.getConexion("sucursal")
+    .then(sqlconnection => {
+      const tabla = sqlconnection.tabla;
+
+      sqlconnection.BD.query(`UPDATE ${tabla} SET ? WHERE id = ?`, [data, id], (err, rows) => {
+        res.redirect('/branch-offices');
+      });
+    })
+    .catch(err => {
+      res.status(500);
+      res.redirect('/branch-offices');
+    })
 }
 
 module.exports = {
