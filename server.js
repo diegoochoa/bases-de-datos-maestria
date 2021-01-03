68193577c288567321dbe9e39e37862ff4952490
diegoochoam <dialogo.crg@gmail.com>
2021-01-03 12:43:50 -0600

    Login

--- server.js
+++ server.js
@@ -2,10 +2,26 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const router = require('./src/routes');
 const mysqlConnection1 = require('./connection');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cookieParser('my secret'));
+app.use(
+  session({
+    secret: 'my secret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.set('view engine', 'ejs');
 
 app.use('/', router);
@@ -28,4 +44,4 @@ module.exports = {
 
 module.exports.stop = () => {
   server.close();
-}
+};
--- src/routes/login-page/login-page-controller.js
+++ src/routes/login-page/login-page-controller.js
@@ -1,10 +1,56 @@
 const express = require('express');
 const app = express();
+const passportLocal = require('passport-local').Strategy;
+const passport = require('passport');
+const mysqlConnection = require('../../../connection');
 
 app.set('view engine', 'ejs');
 
-async function home_page_controller(req, res) {
+passport.use(
+  new passportLocal(function (username, password, done) {
+    let data = null;
+    mysqlConnection
+      .getConexion('usuario')
+      .then((sqlconnection) => {
+        const tabla = sqlconnection.tabla;
+
+        sqlconnection.BD.query(
+          `SELECT * FROM ${tabla} WHERE correo = '${username}' AND contrasena = '${password}'`,
+          (err, rows) => {
+            if (err) console.log(err);
+
+            data = rows[0];
+            console.log(data);
+            if (username === data.correo && password === data.contrasena) {
+              return done(null, { id: 1, name: username });
+            }
+            done(null, false);
+          }
+        );
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  })
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  done(null, { id: 1, name: 'Diego' });
+});
+
+async function login_page_controller(req, res) {
   res.render('login');
 }
 
-module.exports = home_page_controller;
+async function validate_login(req, res) {
+  res.render('home');
+}
+
+module.exports = {
+  login_page_controller,
+  validate_login
+};
